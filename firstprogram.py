52923eea39dcc9b511203757f787a5ad76685656
Ranjan <ranjansingh1114@gmail.com>
2025-02-21 14:42:50 +0530

    new branch added

--- firstprogram.py
+++ firstprogram.py
@@ -1,2 +1,19 @@
 print("hello world")
 print("this is just demo the main picture is still remain")
+
+
+def decorator(func):
+    sum = 0
+
+    def inner(x, y):
+        return func(x, y)
+
+    return inner
+
+
+@decorator
+def addition(x, y):
+    print(x + y)
+
+
+addition(4, 7)
