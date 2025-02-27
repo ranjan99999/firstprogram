81721a48cbe40467182085f06bad28f9cbc8f18e
Ranjan <ranjansingh1114@gmail.com>
2025-02-27 19:51:30 +0530

    Merge remote-tracking branch 'origin/main' into Master_branch
    # Conflicts:
    #	firstprogram.py

--- firstprogram.py
+++ firstprogram.py
@@ -17,3 +17,10 @@ def addition(x, y):
 
 
 addition(4, 7)
+
+
+my_dict = {"a":"ranjan","b":123,"c":"shiv","d":98990 ,"e":"God"}
+for index , item in my_dict.items():
+    if isinstance(item,str):
+        my_dict[index] = item[::-1]
+print(my_dict)
